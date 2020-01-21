5ce3435da237b9a8d900adb4ef2a975c51163689
Maicon Gabriel Friedel <maicon.friedel@kyly.com.br>
2020-01-21 13:42:43 -0300

    Initial Commit

---
+++ src/components/Artists/index.js
@@ -0,0 +1,87 @@
+import React from "react";
+import {
+  View,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  Text,
+  Alert
+} from "react-native";
+
+import { Ionicons } from "@expo/vector-icons";
+
+export default function Artists({ artists }) {
+  function handleAddArtist(artist) {
+    Alert.alert(artist.name, artist.name);
+  }
+
+  return (
+    <View style={styles.artistList}>
+      <FlatList
+        data={artists}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Image
+              source={{ uri: item.picture_big }}
+              style={styles.artistImage}
+              key={item.id}
+            />
+            <Text style={styles.artistName}>{item.name}</Text>
+            <View style={styles.addArtistButtonContainer}>
+              <TouchableOpacity
+                style={styles.addArtistButton}
+                onPress={() => handleAddArtist(item)}
+              >
+                <Text style={styles.addArtistButtonText}>
+                  <Ionicons size={28} name="ios-add" />
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  artistList: {
+    marginTop: 50,
+    marginBottom: 50
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    height: 65
+  },
+  artistImage: {
+    width: 60,
+    height: 60,
+    borderRadius: 30
+  },
+  artistName: {
+    color: "#fff",
+    marginLeft: 10,
+    fontSize: 15,
+    maxWidth: 250
+  },
+  addArtistButtonContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    flex: 1
+  },
+  addArtistButton: {
+    backgroundColor: "#5f47ad",
+    width: 50,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 25,
+    margin: 2
+  },
+  addArtistButtonText: {
+    color: "#fff"
+  }
+});
---
+++ src/index.js
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  StatusBar,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+  Keyboard
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+import Artists from "./components/Artists";
+
+import api from "./services/api";
+
+export default function Main() {
+  const [text, setText] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  async function handleBandSearch(e) {
+    Keyboard.dismiss();
+    const response = await api.get(`/search/artist?q=${text}`);
+
+    setArtists(response.data.data);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.bandSearchContainer}>
+        <TextInput
+          style={styles.bandSearchInput}
+          value={text}
+          onChangeText={setText}
+          placeholder="Digite a banda à pesquisar..."
+          autoCapitalize="words"
+        />
+        <TouchableOpacity
+          title="Pesquisar"
+          style={styles.bandSearchButton}
+          onPress={handleBandSearch}
+        >
+          <Text style={{ color: "#fff" }}>
+            <FontAwesome name="search" size={20} />
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <Artists artists={artists} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+    padding: 10
+  },
+  bandSearchContainer: {
+    flexDirection: "row",
+    top: StatusBar.currentHeight + 10
+  },
+  bandSearchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    fontSize: 16,
+    shadowColor: "#000",
+    shadowOpacity: 0.2,
+    shadowOffset: {
+      width: 4,
+      height: 4
+    },
+    elevation: 3
+  },
+  bandSearchButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#5f47ad",
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+    marginLeft: 5
+  }
+});
